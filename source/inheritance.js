d0cadd81f35d49e7e5287aa59e6d2310fb513686
Liviu Iancu <liviuiancu@gmx.com>
2016-09-02 21:06:27 +0300

    Added another inheritance example

--- source/inheritance.js
+++ source/inheritance.js
@@ -1,6 +1,38 @@
 // inheritance oop
-var beer = {
-   company: 'Grolsch'
+// var beer = {
+//    company: 'Grolsch'
+// };
+// var prototype = Object.getPrototypeOf(beer);
+// console.log(prototype === Object.prototype);
+
+// // adding properties to the native js objects is not recommended
+// Object.prototype.add = function(value) {
+//    return this + value;
+// };
+
+// var empty = {};
+// for (var property in empty) {
+//    if (empty.hasOwnProperty(property)) {
+//       console.log(property);
+//    }
+// }
+
+var person1 = {
+   name: 'Carl Sagan',
+   sayName: function () {
+       console.log(this.name);
+   }
 };
-var prototype = Object.getPrototypeOf(beer);
-console.log(prototype === Object.prototype);
+var person2 = Object.create(person1, {
+   name:{
+      configurable: true,
+      ennumerable: true,
+      value: 'Nikola Tesla',
+      writable: true
+   }
+});
+person1.sayName();
+person2.sayName();
+console.log(person1.hasOwnProperty('sayName'));
+console.log(person1.isPrototypeOf(person2));
+console.log(person2.hasOwnProperty('sayName'));
